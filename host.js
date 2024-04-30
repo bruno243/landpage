9389dd365d329d50eca976e49618371d3d066228
buno243 <bruno.g.m235@gmail.com>
2024-04-30 17:19:27 -0300

    adicionei no js

--- host.js
+++ host.js
@@ -5,3 +5,12 @@ menu.addEventListener('click', () => {
     menu.classList.toggle("ativo");
     navmenu.classList.toggle('ativo');
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.querySelector('.menu');
+    const navMenu = document.querySelector('.navigation ul');
+
+    menu.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+});
